07a6908a44ab5c1b03cd5c0312904e70c64e4b9a
32kda <dmitry.astu@gmail.com>
2023-09-13 10:07:34 +0700

    Work on missle collision

--- weapons/x_missle.gd
+++ weapons/x_missle.gd
@@ -8,12 +8,14 @@ var acceleration = Vector3.ZERO
 var new_vec:Vector3 = Vector3.ZERO
 const MANEURABILITY = 10;
 const MANEUR_MIN_SPEED = 10;
+var rays : Array[RayCast3D]
 
 func start(_transform, _acceleration: Vector3, _linear_velocity: Vector3):
 	global_transform = _transform
 	linear_velocity = _linear_velocity
 	acceleration = _acceleration.normalized();
 	Events.missle_target.connect(self.retarget)	
+	rays.append_array([$Ray1, $Ray2, $Ray3, $Ray4])
 	#velocity = _acceleration * speed
 
 func retarget(target:Vector3)->void:
@@ -38,12 +40,13 @@ func _on_Lifetime_timeout():
 	queue_free()
 
 func _on_Missle_body_entered(body):	
-	if ($Ray.is_colliding()):
-		var collision_point = $Ray.get_collision_point();
-		var collider = $Ray.get_collider()
-		var normal = $Ray.get_collision_normal()
-		queue_free()
-		Events.explosion.emit(collision_point)
+	for ray in rays:
+		if (ray.is_colliding()):
+			var collision_point = ray.get_collision_point();
+			var collider = ray.get_collider()
+			var normal = ray.get_collision_normal()
+			queue_free()
+			Events.explosion.emit(collision_point)
 	
 	
 	
