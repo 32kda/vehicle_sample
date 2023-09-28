f3f11ed8426d4938a52955c6d702a64eb7e99d06
32kda <dmitry.astu@gmail.com>
2023-09-28 23:51:30 +0700

    Work on missle smoke

--- weapons/x_missle.gd
+++ weapons/x_missle.gd
@@ -1,6 +1,5 @@
 extends RigidBody3D
 
-
 @export var speed = 350
 
 #var velocity = Vector3.ZERO
@@ -8,15 +7,12 @@ var acceleration = Vector3.ZERO
 var new_vec:Vector3 = Vector3.ZERO
 const MANEURABILITY = 10;
 const MANEUR_MIN_SPEED = 10;
-var rays : Array[RayCast3D]
 
 func start(_transform, _acceleration: Vector3, _linear_velocity: Vector3):
 	global_transform = _transform
 	linear_velocity = _linear_velocity
 	acceleration = _acceleration.normalized();
 	Events.missle_target.connect(self.retarget)	
-	rays.append_array([$Ray1, $Ray2, $Ray3, $Ray4])
-	#velocity = _acceleration * speed
 	
 func _integrate_forces(state):
 	for i in state.get_contact_count():
@@ -35,14 +31,6 @@ func _physics_process(delta):
 	if linear_velocity.length() > MANEUR_MIN_SPEED:
 		acceleration = lerp(acceleration, new_vec, MANEURABILITY * delta)	
 		look_at(linear_velocity + global_position, Vector3.UP, true)
-	
-		
-	#global_transform = global_transform.looking_at(acceleration)
-	#velocity += (acceleration * delta) as Vector3
-	#velocity = velocity.limit_length(speed)
-	#rotation = velocity.angle_to()
-	#position += velocity * delta
-
 
 func _on_Lifetime_timeout():
 	queue_free()
