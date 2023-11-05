9add23a803db9a8e1aec36b21786fb1f9a18ebe2
32kda <dmitry.astu@gmail.com>
2023-11-05 22:49:11 +0700

    Turret rotation work

--- lc_80.gd
+++ lc_80.gd
@@ -44,12 +44,20 @@ func _process(delta):
 	var kmh = current_speed_mps * 3.6
 	$SpringArm3D/EngineSound.pitch_scale = 1 + kmh / 20
 	Events.emit_signal("player_speed", kmh)
-	var target = $SpringArm3D/RayCast3D.get_collision_point()
+	var target
+	if $SpringArm3D/RayCast3D.is_colliding():
+		target = $SpringArm3D/RayCast3D.get_collision_point()	
+	else: 
+		target = -$SpringArm3D/RayCast3D.global_transform.basis.z * 3000
 	Events.emit_signal("missle_target", target)
 	var local_target = $turret.to_local(target)
 	local_target.y = 0
 	var angle = Vector3.FORWARD.signed_angle_to(local_target, Vector3.UP)
-	$turret.rotation.y += angle * delta * TARGET_YAW_SPEED
+	$turret.rotation.y += angle * clamp(delta * TARGET_YAW_SPEED, 0, 1)
+	var gun_local = $turret/gun.to_local(target)
+	gun_local.x = 0
+	var pitch_angle = Vector3.FORWARD.signed_angle_to(gun_local, Vector3.RIGHT)
+	$turret/gun.rotation.x += pitch_angle * clamp(delta * TARGET_PITCH_SPEED, 0, 1)
 
 func _input(event):
 	if event is InputEventMouseButton and (event as InputEventMouseButton).button_index == MOUSE_BUTTON_RIGHT and event.is_released():
