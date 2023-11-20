292c2f4d6c891bbc80fbb6a709e934cdd2b6e7d1
32kda <dmitry.astu@gmail.com>
2023-11-20 23:40:00 +0700

    Work on name tag

---
+++ name_tag.gd
@@ -0,0 +1,13 @@
+@tool
+extends SubViewport
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	size = $Label.rect_size
+	pass
