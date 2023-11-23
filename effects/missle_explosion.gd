44435dd3852880b74abcb0614e4b80fe9525e369
32kda <dmitry.astu@gmail.com>
2023-11-23 10:01:07 +0700

    Modify landscape a bit, explosions basically functioning

--- effects/missle_explosion.gd
+++ effects/missle_explosion.gd
@@ -1,6 +1,8 @@
 extends Node3D
 
-@export var physical_strength = 500
+@export var physical_strength = 3000
+
+var processed := Dictionary()
 
 var first_run := true
 
@@ -15,18 +17,16 @@ func _process(delta):
 	pass
 				
 func _physics_process(_delta):
-	call_deferred("process_explosion")	
-
-func process_explosion():
-	if first_run:		
-		for body in $Area3D.get_overlapping_bodies():
-			first_run = false
-			if body is RigidBody3D:
+	for body in $Area3D.get_overlapping_bodies():
+		if body is RigidBody3D:
+			var id = body.get_instance_id();
+			if not processed.has(id):
+				processed[id] = true
 				var distance:Vector3 = body.global_transform.origin - global_transform.origin
 				var impact = 1.0 * physical_strength / (distance.length() + 0.1) #Closer the explosion are - bigger the imapct. 0.1 added to avoid zero division problems
 				body.apply_central_impulse(distance.normalized() * impact)
-			if body.has_method("hurt"):
-				pass
+				if body.has_method("hurt"):
+					pass
 
 func _on_audio_stream_player_finished():
 	queue_free()
