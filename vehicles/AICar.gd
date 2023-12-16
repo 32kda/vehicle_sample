6f597d89d88351a8da0d3776e66fa4381af26445
32kda <dmitry.astu@gmail.com>
2023-12-16 14:53:13 +0700

    Car AI work

--- vehicles/AICar.gd
+++ vehicles/AICar.gd
@@ -1,5 +1,18 @@
 extends VehicleBody3D
 
+@export var steer_force = 0.1
+@export var look_ahead = 100
+@export var num_rays = 8
+
+# context array
+var ray_directions = []
+var interest = []
+var danger = []
+
+var chosen_dir = Vector3.FORWARD
+var velocity = Vector3.FORWARD
+var acceleration = Vector3.FORWARD
+
 const LOW_SPEED = 10
 
 var horse_power = 200
@@ -15,11 +28,24 @@ var current_speed_mps = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	interest.resize(num_rays)
+	danger.resize(num_rays)
+	ray_directions.resize(num_rays)
+	for i in num_rays:
+		var angle = i * 2 * PI / num_rays	
+		ray_directions[i] = Vector3.RIGHT.rotated(Vector3.UP, angle)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
+	set_interest()
+	set_danger()
+	choose_direction()
+	var desired_velocity = chosen_dir.rotated(Vector3.UP, rotation) * max_speed
+	velocity = velocity.linear_interpolate(desired_velocity, steer_force)
+	rotation = velocity.angle()
+	move_and_collide(velocity * delta)
+	
 	current_speed_mps = linear_velocity.length()
 	
 	var throt_input = -1.0 #TDOD	
@@ -32,3 +58,40 @@ func _physics_process(delta):
 	
 	var brake_input = 0.0 #TODO
 	brake = lerp(brake, brake_power * brake_input, brake_speed * delta)
+	
+func set_interest():
+	# Set interest in each slot based on world direction
+	if owner and owner.has_method("get_path_direction"):
+		var path_direction = owner.get_path_direction(position)
+		for i in num_rays:
+			var d = ray_directions[i].rotated(rotation).dot(path_direction)
+			interest[i] = max(0, d)
+	# If no world path, use default interest
+	else:
+		set_default_interest()
+
+func set_default_interest():
+	# Default to moving forward
+	for i in num_rays:
+		var d = ray_directions[i].rotated(Vector3.UP, rotation).dot(transform.x)
+		interest[i] = max(0, d)
+
+func set_danger():
+	# Cast rays to find danger directions
+	var space_state = get_world_3d().direct_space_state
+	for i in num_rays:
+			var result = space_state.intersect_ray(position,
+				position + ray_directions[i].rotated(Vector3.UP, rotation) * look_ahead,
+				[self])
+			danger[i] = 1.0 if result else 0.0
+			
+func choose_direction():
+	# Eliminate interest in slots with danger
+	for i in num_rays:
+		if danger[i] > 0.0:
+			interest[i] = 0.0
+	# Choose direction based on remaining interest
+	chosen_dir = Vector3.FORWARD
+	for i in num_rays:
+		chosen_dir += ray_directions[i] * interest[i]
+	chosen_dir = chosen_dir.normalized()
