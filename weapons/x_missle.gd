bdf33694f1077993f7debe98a4e91842a309bf3d
32kda <dmitry.astu@gmail.com>
2024-03-26 23:00:46 +0700

    Work on missle guidance logics

--- weapons/x_missle.gd
+++ weapons/x_missle.gd
@@ -36,19 +36,25 @@ func retarget(target:Vector3, from: Vector3)->void:
 	var to_self:Vector3 = global_position - from
 	dist_from_launcher = to_self.length()
 	var ray:Vector3
+	var arrow_color = Color.RED
 	if dist_from_launcher > MANEUR_MIN_DIST and to_self.length() * TARGET_POINT_COEF < to_target.length():		
 		ray = from + (to_target.normalized() * dist_from_launcher * TARGET_POINT_COEF) - global_position	
+		arrow_color = Color.ORANGE
 		if ray.length() < 1: #if we are too close to beam - just move towards the target to avoid trying to foolow too short vector
 			ray = target - global_position			
+			arrow_color = Color.GREEN
 	else: 
-		ray = target - global_position	
+		ray = target - global_position
+		arrow_color = Color.PURPLE	
 	if debug_draw:
 		DebugDraw3D.draw_sphere(from, 0.3, Color.GREEN)
-		DebugDraw3D.draw_sphere(ray + global_position, 0.5, Color.RED)
+#		DebugDraw3D.draw_arrow_line(from, ray + global_position, Color.RED)
+		#DebugDraw3D.draw_sphere(ray + global_position, 0.5, Color.RED)
 	#var angle = to_target.signed_angle_to(to_self, normal)
 	#print("Angle: " + str(rad_to_deg(angle)))
 	
 	new_vec = ray.normalized() 	
+	DebugDraw3D.draw_arrow_line(from, from + (to_target.normalized() * dist_from_launcher * TARGET_POINT_COEF), arrow_color, 0.4, true)
 
 func _physics_process(delta):
 	apply_central_force(acceleration * speed)
