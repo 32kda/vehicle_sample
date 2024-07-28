52588382c917c0ee92b4a72c4700c2e35242645d
32kda <dmitry.astu@gmail.com>
2024-07-28 23:20:35 +0700

    Investigating collision detection failure, no result yet(

--- weapons/Grenade.gd
+++ weapons/Grenade.gd
@@ -2,6 +2,7 @@ extends RigidBody3D
 
 @onready var collision = $CollisionShape3D
 @onready var raycast = $RayCast3D
+@onready var col_shape = $CollisionShape3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,19 +14,29 @@ func _process(delta):
 	pass
 	
 func _physics_process(delta):
-	if raycast.is_colliding():
+	
+	var query = PhysicsRayQueryParameters3D.create(position, to_global(Vector3.FORWARD))
+	var from_ray = get_world_3d().direct_space_state.intersect_ray(query)
+	if from_ray:
+		print("from_ray " + str(from_ray["collider_id"]))
+		Events.explosion.emit(global_position, from_ray["collider"])
+		destroy()	
+	elif raycast.is_colliding():
+		print("from_ray " + str(from_ray["collider_id"]))
+		Events.explosion.emit(raycast.get_collision_point(), raycast.get_collider())
 		destroy()
 
-func _integrate_forces(state):	
-	for i in state.get_contact_count():
-		var cpos = state.get_contact_collider_position(i)
-		var target = state.get_contact_collider_object(i)
-		print("contact: " + str(target))
-		destroy()
-		Events.explosion.emit(cpos, target)
+#func _integrate_forces(state):	
+#	for i in state.get_contact_count():
+#		var cpos = state.get_contact_collider_position(i)
+#		var target = state.get_contact_collider_object(i)
+#		print("contact: " + str(target))
+#		destroy()
+#		Events.explosion.emit(cpos, target)
 
 func _on_body_entered(body):
-	print(body)
+	Events.explosion.emit(global_position, body)
+	destroy()
 	pass
 	#destroy()
 	#Events.explosion.emit(cpos, target)	
