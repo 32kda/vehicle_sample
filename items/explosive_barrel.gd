7ce6dcb953bb006dd94fa98723398d94745779bb
32kda <dmitry.astu@gmail.com>
2024-09-18 23:44:22 +0700

    Explosive barrel work

--- items/explosive_barrel.gd
+++ items/explosive_barrel.gd
@@ -1,9 +1,11 @@
 extends RigidBody3D
 
 @onready var controller = $HealthController
-
+@onready var boom_timer = $BoomTimer
+var initial_health:int
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	initial_health = controller.get_health()
 	pass # Replace with function body.
 
 
@@ -16,3 +18,11 @@ func hit(damage:int):
 	if controller.is_destroyed():
 		Events.explosion.emit(global_position, null)
 		queue_free()
+	elif controller.get_health() <= initial_health / 2 and boom_timer.is_stopped():
+		$FireParticles.show()
+		boom_timer.start()
+
+
+func _on_boom_timer_timeout():
+	Events.explosion.emit(global_position, null)
+	queue_free()
